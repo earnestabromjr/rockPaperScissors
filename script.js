60e6dd36efae539043084ec2819c13d9a32c1485
earnestabromjr <abromearnest@gmail.com>
2024-02-27 01:19:30 -0600

    Tested script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// function getcomputerChoice
+
+    // create string variable named choice with initial value of "" to hold computers choice 
+
+    // create array named computerChoices to hold type string computer choices
+
+    // Random pick element from computerChoices array
+
+    // assign computer choice to variable choice
+
+    // return choice
+
+// function playerChoice
+
+    // create string variable named playerInput with initial value of "" to hold players choice
+
+    // prompt user to enter their choice
+
+    // use if statement to check if user entered a valid choice
+
+    // assign player choice to variable playerInput
+
+    // return playerInput
