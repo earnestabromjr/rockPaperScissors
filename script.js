ce73073667c9287b8f00ce425d0402d80a194e46
earnestabromjr <abromearnest@gmail.com>
2024-03-01 11:57:34 -0600

    Began defining playRound function Still trying to determine how to return the results or weatehr to console log them or to alert the webpage

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
 // function getcomputerChoice
 function getcomputerChoice() {
-    
-    // create array named computerChoices to hold type string computer choices
-    const computerChoices = ["Rock", "Paper", "Scissors"];
+  // create array named computerChoices to hold type string computer choices
+  const computerChoices = ["Rock", "Paper", "Scissors"];
 
-    // Random pick element from computerChoices array
-    const random = Math.floor(Math.random() * computerChoices.length);
+  // Random pick element from computerChoices array
+  const random = Math.floor(Math.random() * computerChoices.length);
 
-    // assign computer choice to variable choice
-    const choice = computerChoices[random];
+  // assign computer choice to variable choice
+  const choice = computerChoices[random];
 
-    return choice;
+  return choice;
 }
 
 function playerChoice() {
-
-    // prompt user to enter their choice
-    let playerInput = prompt("Please enter your choice: ");
-    playerInput = playerInput.toLowerCase();
-    // use if statement to check if user entered a valid choice
-    while (playerInput !== "rock" && playerInput!== "paper" && playerInput!== "scissors") {
-        alert("Please enter a valid choice");
-        return playerInput;
-    }
-    
+  // prompt user to enter their choice
+  let playerInput = prompt("Please enter your choice: ");
+  playerInput = playerInput.toLowerCase();
+  // use if statement to check if user entered a valid choice
+  while (
+    playerInput !== "rock" &&
+    playerInput !== "paper" &&
+    playerInput !== "scissors"
+  ) {
+    alert("Please enter a valid choice");
     return playerInput;
+  }
+
+  return playerInput;
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+  for (let i = 0; i < 5; i++) {
+    if (playerSelection === computerSelection) {
+      alert("It's a tie!");
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      alert("You won!");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+      alert("You won!");
+    } else if (
+      playerSelection === "scissors" &&
+      computerSelection === "paper"
+    ) {
+      alert("You won!");
+    } else {
+      alert("You lost!");
+    }
+  }
 }
 
 const playerSelection = playerChoice();
 const computerSelection = getcomputerChoice();
-
-
-
